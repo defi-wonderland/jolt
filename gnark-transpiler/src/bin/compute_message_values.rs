3174c308751e173b41e6cbdbce7e9ee09ad7624c
0xParti <102677731+0xParti@users.noreply.github.com>
2026-02-02 17:46:36 -0300

    chore: remove unnecessary allow attributes from utility binaries

--- gnark-transpiler/src/bin/compute_message_values.rs
+++ gnark-transpiler/src/bin/compute_message_values.rs
@@ -1,4 +1,3 @@
-#![allow(dead_code, unused_variables, unused_imports)]
 //! Compute exact message values for Go tests
 //!
 //! Usage: cargo run -p gnark-transpiler --bin compute_message_values --release
